542ef668bab416d3d9d30e18f64492d655cae167
Jonathan Smith <naxlin.everthaine@gmail.com>
2016-03-14 16:44:47 -0700

    Completed lyn5

--- NaxlinTest/lyn5-main.js
+++ NaxlinTest/lyn5-main.js
@@ -1,4 +1,13 @@
-﻿//var fs = require('fs');
+﻿var fs = require('fs');
 var lyn5 = require('./lyn5-mod.js')
-//var dir = process.argv[2];
+var dir = process.argv[2];
+var ext = process.argv[3];
 
+lyn5(dir, ext, function (err, data) {
+    if (err) {
+        return callback(err);
+    };
+    for (i = 0; i < data.length; i++) {
+        console.log(data[i]);
+    };
+});
--- NaxlinTest/lyn5-mod.js
+++ NaxlinTest/lyn5-mod.js
@@ -1,19 +1,13 @@
 ﻿var path = require('path');
 var fs = require('fs');
-var dir = process.argv[2];
 
-module.exports = function(dir, ext, callback){
+module.exports = function (dir, ext, callback){ 
     fs.readdir(dir, function (err, list) {
-    if (err) {
-            return callback(err);
-    };
-    var ext = '.' + ext;
-    for (i = 0; i < list.length; i++) {
-        var ext1 = path.extname(list[i]);
-        if (ext1 === ext) {
-            var item = list[i];
-        };
-    };
-    callback(null, list)
+        ext = '.' + ext;        
+        var list1 = list.filter(function (file) {
+            var x = (ext === path.extname(file));
+            return x;
+        });
+        callback(null, list1);
     })
 };
