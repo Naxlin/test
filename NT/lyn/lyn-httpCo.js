c14338cda2cc6a395f45a984255e4e3207832838
Jonathan Smith <naxlin.everthaine@gmail.com>
2016-03-18 16:25:00 -0700

    Finished lyn http collect.

---
+++ NT/lyn/lyn-httpCo.js
@@ -0,0 +1,27 @@
+﻿var bl = require('bl');
+var http = require('http');
+
+http.get(process.argv[2], function (response) {
+    response.pipe(bl(function (err, data) {
+        console.log(data.length);
+        console.log(data.toString());
+    }));
+    response.on('error', console.error);
+});
+    
+
+/** Their Way:
+var http = require('http')
+var bl = require('bl')
+ 
+http.get(process.argv[2], function (response) {
+    response.pipe(bl(function (err, data) {
+        if (err)
+            return console.error(err)
+        data = data.toString()
+        console.log(data.length)
+        console.log(data)
+    }))
+})
+
+**/
