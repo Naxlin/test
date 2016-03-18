5530a7fc2bf2d2996640b5001c42a715e884080a
Jonathan Smith <naxlin.everthaine@gmail.com>
2016-03-18 15:48:46 -0700

    Finished lyn http client

---
+++ NT/lyn/lyn-httpC.js
@@ -0,0 +1,27 @@
+﻿var http = require('http');
+
+var link = process.argv[2];
+
+http.get(link, function (response) {
+    var data = "";
+    response.on("data", function (chunk) {
+        data += chunk + '\n';
+    });
+    response.on("end", function () {
+        console.log(data);
+    })
+}).on("error", function () {
+    callback(null);
+});
+
+
+/** Their Way:
+var http = require('http')
+
+http.get(process.argv[2], function (response) {
+    response.setEncoding('utf8')
+    response.on('data', console.log)
+    response.on('error', console.error)
+})
+
+**/
