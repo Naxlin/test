143e64c13a008af6a732c6c6d5b61f6ad3e88ab8
Jonathan Smith <naxlin.everthaine@gmail.com>
2016-03-28 14:54:56 -0700

    Finished lyn http uppercaser

--- NT/lyn/lyn-httpUp.js
+++ NT/lyn/lyn-httpUp.js
@@ -1,1 +1,28 @@
-﻿
+﻿var map = require('through2-map');
+var http = require('http');
+var port = process.argv[2];
+
+var server = http.createServer(function (req, res) {
+    req.pipe(map(function (chunk) {
+        return chunk.toString().toUpperCase();
+    })).pipe(res); 
+});
+
+server.listen(port);
+
+/** Their Way:
+var http = require('http')
+var map = require('through2-map')
+
+var server = http.createServer(function (req, res) {
+    if (req.method != 'POST')
+        return res.end('send me a POST\n')
+
+    req.pipe(map(function (chunk) {
+        return chunk.toString().toUpperCase()
+    })).pipe(res)
+})
+
+server.listen(Number(process.argv[2])) 
+
+**/
