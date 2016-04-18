9a8c05b15a928cd699c4cfac989d6c60d59ea465
Jonathan Smith <naxlin.everthaine@gmail.com>
2016-04-18 13:51:17 -0700

    Finished FJS BasicMap.

---
+++ NT/FJS/BasicMap.js
@@ -0,0 +1,14 @@
+﻿module.exports = function doubleAll(numbers) {
+    return numbers.map(function (num) {
+        return num * 2;
+    });
+};
+
+/** Their Way:
+module.exports = function doubleAll(numbers) {
+    return numbers.map(function double(num) {
+    return num * 2
+    })
+}
+
+**/
