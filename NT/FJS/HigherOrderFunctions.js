9f2d808d59484ceec87e56d14d633a6222b639f8
Jonathan Smith <naxlin.everthaine@gmail.com>
2016-04-18 13:30:21 -0700

    Finished FJS HigherOrderFunctions.

---
+++ NT/FJS/HigherOrderFunctions.js
@@ -0,0 +1,21 @@
+﻿
+module.exports = function repeat(operation, num) {
+    if (num < 0) {
+        return console.log("negative number");
+    };
+    if (num === 0) {
+        return console.log("finished");
+    };
+    return repeat(operation, num - 1);
+};
+
+/** Their Way:
+function repeat(operation, num) {
+    if (num <= 0) return
+    operation()
+    return repeat(operation, --num)
+}
+
+module.exports = repeat
+
+**/
