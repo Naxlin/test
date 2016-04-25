70afd61c63477b16a192a1b7ea075c3ef0f45ce2
Jonathan Smith <naxlin.everthaine@gmail.com>
2016-04-25 12:03:40 -0700

    Finished FJS Basic Filter.

---
+++ NT/FJS/BasicFilter.js
@@ -0,0 +1,20 @@
+﻿module.exports = function getShortMessages(messages) {
+    //console.log(messages);
+    return shortMessages = messages.filter(function (mes) {
+        return mes.message.length < 50;
+    }).map(function (mes) {
+        return mes.message;
+    });
+};
+
+
+/** Their Way:
+module.exports = function getShortMessages(messages) {
+    return messages.filter(function(item) {
+        return item.message.length < 50
+    }).map(function(item) {
+        return item.message
+    })
+}
+
+**/
