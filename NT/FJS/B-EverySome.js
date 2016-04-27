6b325e406e519d0db47f36b8138f06f7875db5d2
Jonathan Smith <naxlin.everthaine@gmail.com>
2016-04-27 13:00:15 -0700

    Finished FJS B-EverySome.

---
+++ NT/FJS/B-EverySome.js
@@ -0,0 +1,22 @@
+﻿module.exports = function checkUsersValid(goodUsers) {
+    return function AllUsersValid(submittedUsers) {
+        return submittedUsers.every(function (ele, idx, arr) {
+            return goodUsers.some(function (gele, gidx, garr) {
+                return (ele.id === gele.id);
+            });
+        });
+    };
+};
+
+/** Their Way:
+ module.exports = function checkUsersValid(goodUsers) {
+      return function allUsersValid(submittedUsers) {
+        return submittedUsers.every(function(submittedUser) {
+          return goodUsers.some(function(goodUser) {
+            return goodUser.id === submittedUser.id
+          })
+        })
+      }
+    }
+
+**/
