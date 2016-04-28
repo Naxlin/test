c4c545224212c109ebf734265bafa782d45f8e68
Jonathan Smith <naxlin.everthaine@gmail.com>
2016-04-28 12:53:13 -0700

    Finished FJS B-Reduce.

---
+++ NT/FJS/B-Reduce.js
@@ -0,0 +1,21 @@
+﻿module.exports = function countWords(inputWords) {
+    return inputWords.reduce(function (prev, crnt, idx, ary) {
+        if (isNaN(prev[crnt])) {
+            prev[crnt] = 0;
+        };
+        prev[crnt] = ++prev[crnt];
+        return prev;
+    }, {} );
+};
+
+/** Their Way:
+function countWords(arr) {
+    return arr.reduce(function(countMap, word) {
+    countMap[word] = ++countMap[word] || 1 // increment or initialize to 1
+    return countMap
+    }, {}) // second argument to reduce initialises countMap to {}
+}
+
+module.exports = countWords
+
+**/
