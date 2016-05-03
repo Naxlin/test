1240798913762233de98bd736b3512ec988e9017
Jonathan Smith <naxlin.everthaine@gmail.com>
2016-05-02 22:48:23 -0700

    Finished FJS B-Recursion.

---
+++ NT/FJS/B-Recursion.js
@@ -0,0 +1,19 @@
+﻿module.exports = function reduce(arr, fn, init) {
+    if (!arr.length) return init;
+    fn(init, arr[0], 0, arr)
+    var next = arr.slice(1);
+    var cur = init;
+    return reduce(next, fn, cur);
+};
+
+/** Their Way:
+function reduce(arr, fn, initial) {
+    return (function reduceOne(index, value) {
+        if (index > arr.length - 1) return value // end condition
+        return reduceOne(index + 1, fn(value, arr[index], index, arr)) // calculate & pass values to next step
+    })(0, initial) // IIFE. kick off recursion with initial values
+}
+
+module.exports = reduce
+
+**/
