81532cddf58ec050746b545860670f91d99d6d71
Jonathan Smith <naxlin.everthaine@gmail.com>
2016-06-04 14:07:35 -0700

    Finished ImplimentReducedMap.

--- NT/FJS/ImplimentReducedMap.js
+++ NT/FJS/ImplimentReducedMap.js
@@ -1,3 +1,17 @@
-﻿module.exports = function arrayMap(arr, fn) {
-    return input * arr.reduce();
+﻿module.exports = function arrayMap(arr, fn, thisArg) {
+    return arr.reduce(function (newarr,curr,idx,arr) {
+        newarr.push(fn.call(thisArg, curr));
+        return newarr;
+    }, []);
 };
+
+
+/** Their Way:
+module.exports = function arrayMap(arr, fn, thisArg) {
+    return arr.reduce(function(acc, item, index, arr) {
+    acc.push(fn.call(thisArg, item, index, arr))
+    return acc
+    }, [])
+}
+
+**/
