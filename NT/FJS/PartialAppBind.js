dbca33fbbb7264180bede1f616a024be5868ff3f
Jonathan Smith <naxlin.everthaine@gmail.com>
2016-06-03 17:41:53 -0700

    Finished PartialAppBind.

---
+++ NT/FJS/PartialAppBind.js
@@ -0,0 +1,17 @@
+﻿var slice = Array.prototype.slice;
+
+module.exports = function logger(namespace){
+    return function (){
+        var fix = logger.bind(undefined, namespace);
+        var log = fix(slice.apply(arguments).join(" "))
+        return console.log(log);
+    }
+};
+
+
+/** Their Way:
+module.exports = function (namespace) {
+    return console.log.bind(console, namespace)
+}
+
+**/
