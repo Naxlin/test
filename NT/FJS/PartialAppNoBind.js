cdf1b12950c91ceebabf7bc293701fe8907e2fe8
Jonathan Smith <naxlin.everthaine@gmail.com>
2016-06-03 17:24:22 -0700

    Finished PartialAppNoBind.

--- NT/FJS/PartialAppNoBind.js
+++ NT/FJS/PartialAppNoBind.js
@@ -1,5 +1,22 @@
 ﻿var slice = Array.prototype.slice;
 
-module.export = function logger(namespace){
-    console.log('namespace', );
+module.exports = function logger(namespace){
+    return function (){
+        var args = slice.apply(arguments);
+        return console.log(namespace, args.join(" "));
+    }
 };
+
+
+/** Their Way:
+var slice = Array.prototype.slice
+
+function logger(namespace) {
+    return function() {
+    console.log.apply(console, [namespace].concat(slice.call(arguments)))
+    }
+}
+
+module.exports = logger
+
+**/
