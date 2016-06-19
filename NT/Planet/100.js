edab1b387c0226eb2726d049036e4d28b78dacc9
Jonathan Smith <naxlin.everthaine@gmail.com>
2016-06-18 18:02:02 -0700

    Finished PlanetProto.

--- NT/Planet/100.js
+++ NT/Planet/100.js
@@ -3,43 +3,38 @@
 // -> Both robby and cranky should respond to 'parts' and 'capabilities', these 
 //    should be empty arrays at first
 
-function Robot() { /**this.parts = []; **/this.capabilities = []};
+function Robot() { this.parts = [] };//this.capabilities = []};
 
-Robot.prototype.parts = [];
+Robot.prototype.capabilities = [];
 
 var robby = new Robot();
 var cranky = new Robot();
 
-robby.parts.push('core');
+// -> Claim the result of robby.parts
+claim(robby.parts, []);
+// -> Claim the result of cranky.parts
+claim(cranky.parts, []);
+// -> Claim the result of robby.capabilities
+claim(robby.capabilities, []);
+// -> Claim the result of cranky.capabilities
+claim(cranky.capabilities, []);
 
-console.log(cranky.parts);
+// -> Add 'core' to robby.parts, cranky.parts should still be empty
+// -> Add 'fly' to robby.capabilities, after doing that cranky.capabilities must 
+//    also have 'fly' without adding to it directly, so this property has to be 
+//    shared
 
-//// -> Claim the result of robby.parts
-//claim(robby.parts, []);
-//// -> Claim the result of cranky.parts
-//claim(cranky.parts, []);
-//// -> Claim the result of robby.capabilities
-//claim(robby.capabilities, []);
-//// -> Claim the result of cranky.capabilities
-//claim(cranky.capabilities, []);
+robby.parts = ['core'];
+robby.capabilities.push('fly');
 
-//// -> Add 'core' to robby.parts, cranky.parts should still be empty
-//// -> Add 'fly' to robby.capabilities, after doing that cranky.capabilities must 
-////    also have 'fly' without adding to it directly, so this property has to be 
-////    shared
-
-//robby.parts.push('core');
-//Robot.prototype.capabilities = ['fly'];
-
-//// -> Claim the result of robby.parts
-//claim(robby.parts, ['core']);
-//// -> Claim the result of cranky.parts
-//claim(cranky.parts, []);
-//// -> Claim the result of robby.capabilities
-//claim(robby.capabilities, ['fly']);
-//// -> Claim the result of cranky.capabilities
-//claim(cranky.capabilities, ['fly']);
-
+// -> Claim the result of robby.parts
+claim(robby.parts, ['core']);
+// -> Claim the result of cranky.parts
+claim(cranky.parts, []);
+// -> Claim the result of robby.capabilities
+claim(robby.capabilities, ['fly']);
+// -> Claim the result of cranky.capabilities
+claim(cranky.capabilities, ['fly']);
 
 // ------------------------------------------------
 // Common JS exports for verification, don't modify
@@ -48,3 +43,35 @@ module.exports = {
 	robby:  robby,
 	cranky: cranky
 }
+
+
+/** Their Ways:
+function Robot() {
+    this.parts = [];
+}
+
+Robot.prototype.capabilities = [];
+
+var robby = new Robot();
+var cranky = new Robot();
+
+claim(robby.parts, []);
+claim(cranky.parts, []);
+claim(robby.capabilities, []);
+claim(cranky.capabilities, []);
+
+robby.parts.push('core');
+robby.capabilities.push('fly');
+
+claim(robby.parts, ['core']);
+claim(cranky.parts, []);
+claim(robby.capabilities, ['fly']);
+claim(cranky.capabilities, ['fly']);
+
+module.exports = {
+    Robot:  Robot,
+    robby:  robby,
+    cranky: cranky
+}
+
+**/
