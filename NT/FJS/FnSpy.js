3a5ae9d2bf1730526d934ed5eb818fd1d7e18db0
Jonathan Smith <naxlin.everthaine@gmail.com>
2016-06-18 17:39:37 -0700

    PlanetProto

---
+++ NT/FJS/FnSpy.js
@@ -0,0 +1,3 @@
+﻿module.exports = function Spy(target, method) {
+
+};
---
+++ NT/Planet/010.js
@@ -0,0 +1,26 @@
+// -> Create an object called 'robot' using an object literal
+// -> robot should have a property 'smart' with value true
+var robot = { smart: true };
+
+// -> Claim the result robot.smart
+claim(robot.smart, true);
+
+// ------------------------------------------------
+// Common JS exports for verification, don't modify
+module.exports = {
+    robot: robot
+};
+
+
+/** Their Way
+var robot = {
+    smart: true
+}
+
+claim(robot.smart, true);
+
+module.exports = {
+    robot: robot
+}
+
+**/
