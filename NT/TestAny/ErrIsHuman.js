79dbae843c452c950d73243b7bf1ad9a0b74df02
Jonathan Smith <naxlin.everthaine@gmail.com>
2016-06-22 21:38:58 -0700

    Finished test-anything.

---
+++ NT/TestAny/ErrIsHuman.js
@@ -0,0 +1,21 @@
+﻿var feedCat = require(process.argv[2]);
+var test = require('tape');
+
+test('Feeding Cats.', function (t) {
+    t.plan(2);
+    t.equal(feedCat('food'), 'yum', "food should return yum!");
+    t.throws(feedCat.bind(null, 'chocolate'));
+});
+
+
+/** Their Way:
+var test = require('tape')
+var feedCat = require(process.argv[2])
+
+test('cat feeding', function (t) {
+    t.plan(2)
+    t.equal(feedCat('food'), 'yum')
+    t.throws(feedCat.bind(null, 'chocolate'))
+})
+
+**/
